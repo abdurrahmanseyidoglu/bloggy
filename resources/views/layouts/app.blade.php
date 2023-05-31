a949aa20fa51433b45b76e300b11a84a3164e92c
Abdurrahman Seyidoglu <abdurrahmanseyidoglu@gmail.com>
2023-05-31 23:17:57 +0300

    Clean up home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,18 +31,12 @@
 
         <nav>
             <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
-                <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Shop</a></li>
                 <li><a class="hover:text-gray-200 hover:underline px-4" href="#">About</a></li>
             </ul>
         </nav>
 
         <div class="flex items-center text-lg no-underline text-white pr-6">
-            <a class="" href="#">
-                <i class="fab fa-facebook"></i>
-            </a>
-            <a class="pl-6" href="#">
-                <i class="fab fa-instagram"></i>
-            </a>
+
             <a class="pl-6" href="#">
                 <i class="fab fa-twitter"></i>
             </a>
@@ -58,10 +52,10 @@
 <header class="w-full container mx-auto">
     <div class="flex flex-col items-center py-12">
         <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
-            Minimal Blog
+            Personal Blog
         </a>
         <p class="text-lg text-gray-600">
-            Lorem Ipsum Dolor Sit Amet
+            I write what I think is useful for me and other people
         </p>
     </div>
 </header>
@@ -80,12 +74,12 @@
     <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
         <div
             class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
-            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">PHP</a>
+            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Other tags goes here</a>
+            {{--            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>--}}
+            {{--            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>--}}
+            {{--            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>--}}
+            {{--            <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>--}}
         </div>
     </div>
 </nav>
@@ -108,83 +102,41 @@
             </a>
         </div>
 
-        <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-            <p class="text-xl font-semibold pb-5">Instagram</p>
-            <div class="grid grid-cols-3 gap-3">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
-                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
-            </div>
-            <a href="#"
-               class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
-            </a>
-        </div>
+        {{--        <div class="w-full bg-white shadow flex flex-col my-4 p-6">--}}
+        {{--            <p class="text-xl font-semibold pb-5">Instagram</p>--}}
+        {{--            <div class="grid grid-cols-3 gap-3">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=1">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=2">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=3">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">--}}
+        {{--                <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">--}}
+        {{--            </div>--}}
+        {{--            <a href="#"--}}
+        {{--               class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">--}}
+        {{--                <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb--}}
+        {{--            </a>--}}
+        {{--        </div>--}}
 
     </aside>
 
 </div>
 
 <footer class="w-full border-t bg-white pb-12">
-    <div
-        class="relative w-full flex items-center invisible md:visible md:pb-12"
-        x-data="getCarouselData()"
-    >
-        <button
-            class="absolute bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 ml-12"
-            x-on:click="decrement()">
-            &#8592;
-        </button>
-        <template x-for="image in images.slice(currentIndex, currentIndex + 6)" :key="images.indexOf(image)">
-            <img class="w-1/6 hover:opacity-75" :src="image">
-        </template>
-        <button
-            class="absolute right-0 bg-blue-800 hover:bg-blue-700 text-white text-2xl font-bold hover:shadow rounded-full w-16 h-16 mr-12"
-            x-on:click="increment()">
-            &#8594;
-        </button>
-    </div>
     <div class="w-full container mx-auto flex flex-col items-center">
         <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
-            <a href="#" class="uppercase px-3">About Us</a>
-            <a href="#" class="uppercase px-3">Privacy Policy</a>
-            <a href="#" class="uppercase px-3">Terms & Conditions</a>
-            <a href="#" class="uppercase px-3">Contact Us</a>
+            <a href="#" class="uppercase px-3">About</a>
+            {{--            <a href="#" class="uppercase px-3">Privacy Policy</a>--}}
+            {{--            <a href="#" class="uppercase px-3">Terms & Conditions</a>--}}
+            <a href="#" class="uppercase px-3">Contact</a>
         </div>
         <div class="uppercase pb-6">&copy; myblog.com</div>
     </div>
 </footer>
 
-<script>
-    function getCarouselData() {
-        return {
-            currentIndex: 0,
-            images: [
-                'https://source.unsplash.com/collection/1346951/800x800?sig=1',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=2',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=3',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=4',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=5',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=6',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=7',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=8',
-                'https://source.unsplash.com/collection/1346951/800x800?sig=9',
-            ],
-            increment() {
-                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex + 1;
-            },
-            decrement() {
-                this.currentIndex = this.currentIndex === this.images.length - 6 ? 0 : this.currentIndex - 1;
-            },
-        }
-    }
-</script>
 
 </body>
 </html>
